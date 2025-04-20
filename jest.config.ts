f4809b1ab53bdf745c116bd8c27f21f4094779d4
GregoriSimei <gregorisimei@gmail.com>
2025-04-19 22:23:20 -0300

    chore: configure jest

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.spec.ts', '**/?(*.)+(spec).ts'],
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    clearMocks: true,
+    moduleNameMapper: {
+      '^@config/(.*)$': '<rootDir>/src/config/$1',
+      '^@application/(.*)$': '<rootDir>/src/application/$1',
+      '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+      '^@infra/(.*)$': '<rootDir>/src/infra/$1',
+      '^@vars/(.*)$': '<rootDir>/src/config/variables/$1',
+      '^@test/(.*)$': '<rootDir>/src/tests/$1',
+    },
+    setupFiles: ['<rootDir>/jest.setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/src/tests/'],
+    coveragePathIgnorePatterns: ['<rootDir>/src/tests/'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+  };
